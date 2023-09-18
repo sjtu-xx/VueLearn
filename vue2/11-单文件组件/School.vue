31fac9ca4012897a84f01b40b940fda547cf6f28
xuexuan <1099313374@qq.com>
2023-09-18 23:25:08 +0800

    单文件组件

---
+++ vue2/11-单文件组件/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+
+<script>
+
+// export default Vue.extend({})
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: 'school'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
